fccec6910d9034f66157c8c45e29b83d577f69a4
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-01 20:17:58 +0500

    First file

---
+++ Zadasha1/Program.cs
@@ -0,0 +1,29 @@
+﻿// проверка на некоректность ввода
+int GetNumber(string message)
+{
+    bool isCorrect = false;
+    int result = 0;
+
+    while(!isCorrect)
+    {
+        Console.WriteLine(message);
+        if(int.TryParse(Console.ReadLine(), out result))
+        {
+            return result;
+        }
+        else
+            Console.WriteLine("Введите не число");
+    }
+
+    return result;
+}
+
+
+
+
+// квадрат числа
+int number = GetNumber("Введите число");
+
+int result = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {result}");
