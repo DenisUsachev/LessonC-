b7c6daa75cc62a1048c415ccb47666ab08a8d4b3
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-02 18:49:38 +0500

    add second file

---
+++ Zadasha2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите квадрат числа ");
+string input0 = Console.ReadLine();
+Console.WriteLine("Введите число");
+string input1 = Console.ReadLine();
+int num0 = Convert.ToInt32(input0);
+int num1 = Convert.ToInt32(input1);
+
+if ((num1 * num1) == num0 )
+{
+    Console.WriteLine($"Число {num1} является квадратом корня {num0}");
+}
+else
+{
+    Console.WriteLine($"Число {num1} не является квадратом корня {num0}");
+}
