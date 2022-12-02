210c315fc75bb26c75965ec1276754075db9aa21
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-02 21:23:30 +0500

    add file

---
+++ Zadasha7/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трёхзначное число ");
+string num0 = Console.ReadLine()??"";
+int number = int.TryParse(number, out int n);
+if((number > 99) && (number <= 999))
+{
+    Console.Write(number % 10);
+}
+else
+    Console.Write("Неверно введенное число");
